d2d93448eb5e049b64aa3ac80dd2c26e29e3e89b
Ty W <71028905+s10486025@users.noreply.github.com>
2022-01-24 08:49:19 -0500

    v0.1

---
+++ ArrayIntro.java
@@ -0,0 +1,42 @@
+public class ArrayIntro {
+    public static void main (String[] args){
+        //DataType[] name = new Datatype[size]
+        String[] strArr = new String[10];
+        int[] intArr = new int[10];
+        double[] doubArr = new double[10];
+
+
+        //only goes up to number 9 for indexes
+        strArr[0] = "dog";
+        strArr[1] = "cat";
+        strArr[2] = "panda";
+        strArr[3] = "lovebug";
+        strArr[4] = "bedbugs";
+        strArr[5] = "armadillo";
+        strArr[7] = "doves";
+        strArr[8] = "grasshopper";
+        strArr[9] = "grapple";
+
+        intArr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        doubArr = {1.5, 2.6, 7, 8.9, 9.5, 10.2, 12.3, 16.3 20.6, 22.4};
+    }
+
+    public static void stringmethod(){
+        for(int i =0; i < 10; i++){
+            String currentString = strArr[i];
+            int stringLength = currentString,length();
+
+        }
+
+    }
+
+    public static void intMeithod(){
+
+    }
+
+    public static void doubleMethod(){
+
+    }
+
+}
---
+++ JacksonStanton00.java
@@ -0,0 +1,11 @@
+// ty Whitecotten, Array Practice, v0.1, 1/24/22
+
+public class JacksonStanton00 {
+
+    public static void main(String[] args) {
+
+        String[] carArr = {"BMW M5", "Ferrari 250 GTO", "Porshe 911", "Ford F150", "buick Skylark"};
+        int[] myGrades = {100, 25, 15, -3, 0, 75, 35};
+        double[] myBankAccount = {0.0, -1.25, 3.75, 456.99, 25.25, 12.123456, 3.14};
+    }
+}
