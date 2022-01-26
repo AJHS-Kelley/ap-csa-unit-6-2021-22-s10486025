9ad0ad8394ccc722ca6f7e7116dab1934ac42d7f
Ty W <71028905+s10486025@users.noreply.github.com>
2022-01-26 08:34:21 -0500

    v0.3

--- JacksonStanton00.java
+++ JacksonStanton00.java
@@ -1,10 +1,10 @@
-// ty Whitecotten, Array Practice, v0.2, 1/24/22
+// ty Whitecotten, Array Practice, v0.3, 1/24/22
 
 public class JacksonStanton00 {
 
     public static void main(String[] args) {
 
-        
+        String[] carArr = {"BMW M5", "Ferrari 250 GTO", "Porshe 911", "Ford F150", "buick Skylark"};
         int[] myGrades = {100, 25, 15, -3, 0, 75, 35};
         double[] myBankAccount = {0.0, -1.25, 3.75, 456.99, 25.25, 12.123456, 3.14};
         /*
@@ -15,19 +15,25 @@ public class JacksonStanton00 {
             // Statement 3 is executed EACH TIME THE LOOP RUNS.
 
         }
-        
-        for (i =0; i < carArr.length(); i++) {
-
-        }
         */
 
 
+        for (int i =0; i < carArr.length; i++) {
+            //System.out.println(i);
+            System.out.println(carArr[i]);
 
-        String[] carArr = {"BMW M5", "Ferrari 250 GTO", "Porshe 911", "Ford F150", "buick Skylark"};
+        }
+        
+
+
+
+        
         System.out.println(carArr[3]);
         // Once created you cannot change the size of an array.
         carArr[0] = "Volkswagen Passat";
 
+
+        // Enhanced for loop
         for (String eachElement : carArr ) {
             // Code to execute.
             System.out.println(eachElement);
